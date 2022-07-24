f7c5fc38d6e81d3c96b8ba4ce6c1e3ed47c83397
reznikovas <ramdirt@mail.ru>
2022-07-24 20:36:26 +0300

    добавлен контейнер зависимостей, обновлены образы

--- api/public/index.php
+++ api/public/index.php
@@ -2,13 +2,28 @@
 
 declare(strict_types=1);
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+http_response_code(500);
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new \DI\ContainerBuilder();
+
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ]
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("{}");
